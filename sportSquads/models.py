02e389dd58b187b8a949fe1ee469052bc46553c1
Gary Allan <2542931a@student.gla.ac.uk>
2022-03-03 19:50:31 +0000

    Make use of django User + on_delete=SET()

--- sportSquads/models.py
+++ sportSquads/models.py
@@ -1,29 +1,29 @@
+import django.contrib.auth.models
 from django.db import models
 from django.template.defaultfilters import slugify
+from django.contrib.auth.models import User
+from django.conf import settings
+from django.contrib.auth import get_user_model
 
 
-class User(models.Model):
-    user_id = models.AutoField(primary_key=True)
-    username = models.CharField(max_length=64)
-    password = models.CharField(max_length=128)
-    email = models.EmailField()
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     profile_picture = models.ImageField(blank=True)
     bio = models.TextField()
-    username_slug = models.SlugField(unique=True)   # username appears in some urls so slugging it
-
-    def save(self, *args, **kwargs):
-        self.username_slug = slugify(self.username)
-        super(User, self).save(*args, **kwargs)
 
     def __str__(self):
-        return self.username
+        return self.user.username
+
+
+def get_sentinel_user():
+    return get_user_model().objects.get_or_create(username="deleted")[0]
 
 
 class Sport(models.Model):
     name = models.CharField(max_length=64, unique=True)
     image = models.ImageField(blank=True)
     description = models.TextField()
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET(get_sentinel_user()))
     roles = models.JSONField()
     name_slug = models.SlugField(unique=True)   # sport name appears in some urls so slugging it
 
@@ -44,8 +44,8 @@ class Team(models.Model):
     sport = models.ForeignKey(Sport, on_delete=models.CASCADE)
 
     # related name is so that django can distinguish between the two FKs
-    manager = models.ForeignKey(User, on_delete=models.CASCADE, related_name="manager")
-    members_with_roles = models.ManyToManyField(User, through="TeamUserMembership", related_name="member_with_roles")
+    manager = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name="manager")
+    members_with_roles = models.ManyToManyField(UserProfile, through="TeamUserMembership", related_name="member_with_roles")
 
     available_roles = models.JSONField()
     name_slug = models.SlugField(unique=True)   # team name appears in some urls so slugging it
@@ -62,7 +62,7 @@ class Team(models.Model):
 # this class is so that we can have the (member, role) in the Team class
 # could add more fields here such as date joined, reason for joining etc.
 class TeamUserMembership(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
     team = models.ForeignKey(Team, on_delete=models.CASCADE)
 
     role = models.CharField(max_length=64)
