ee25923779e8522d144bb402d435a3ff8f2e6e71
Sophia Karamichali <2570646k@student.gla.ac.uk>
2022-03-05 18:24:35 +0000

    for loop testing

--- sportSquads/views.py
+++ sportSquads/views.py
@@ -11,12 +11,11 @@ from sportSquads.models import Team
 
 from django.urls import reverse
 
-# Create your views here.
 
 def home(request):
 
-    sport_list =  Sport.objects
-    team_list =  Team.objects
+    sport_list = Sport.objects.all()
+    team_list =  Team.objects.all()
     context_dict = {}
     # key boldmessage matches to {{ boldmessage }} in the template
     #context_dict['boldmessage']='Crunchy, creamy, cookie, candy, cupcake!'
@@ -26,8 +25,36 @@ def home(request):
     # We make use of the shortcut function to make our lives easier.
     # Note that the first parameter is the template we wish to use.
 
-
     
     response = render(request, "sportSquads/home.html", context=context_dict)
 
     return response
+
+def show_sport(request,sport_name_slug):
+    #context dictionary which we can pass to the template rendering engine
+    context_dict = {}
+
+    try:
+        sport = Sport.objects.get(slug=sport_name_slug)
+
+        #retrieve all of the associated pages.
+        #the filter() will return a list of page objects or an empty list
+        teams = team.objects.filter(team=team)
+
+        #Adds our results list to the template context under name pages.
+        context_dict['teams'] = teams
+        #We also add the category objects from
+        #the database to the context dictionary
+        #We'll use this in the template to verify that the category exists.
+        context_dict['teams'] = teams
+        
+    except Category.DoesNotExist:
+        #we get here if we didnt find the specified category
+        #dont do anything -
+        #the template will display "no categort" message
+        context_dict['sport']=None
+        context_dict['teams']=None
+
+    #Go render the response and return it to the client.
+    return render(request, 'sportSquads/sport.html', context=context_dict)
+
--- sportSquads_project/urls.py
+++ sportSquads_project/urls.py
@@ -20,4 +20,6 @@ from sportSquads import views
 urlpatterns = [
     #path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    path('sport/<slug:sport_name_slug>/',
+         views.show_sport, name='show_sport'),
 ]
