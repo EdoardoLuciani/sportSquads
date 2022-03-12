0a519cc62cb100aea3932adee8f637c96f9c7db4
Edoardo Luciani <edoardo.luciani@gmail.com>
2022-03-12 01:09:05 +0000

    Add js to dynamically generate Sport roles fields

---
+++ static/js/add_new_sport.js
@@ -0,0 +1,12 @@
+$(document).ready(function() { 
+
+    $('#add_new_sport').on('keypress', 'p:nth-last-of-type(2)', function() {
+        if ($(this).find('input').val().length > 0) {
+            var idx = parseInt($(this).find('label').text().slice(5).slice(0,-1)) + 1;
+    
+            var last_elem = $(this).next().after('<p> <label for="id_role_'+ idx +'">Role '+ idx +':</label> <input type="text" name="role_'+ idx +'" maxlength="64" id="id_role_'+ idx +'"> </p>');
+            $(last_elem).next().after('<p> <label for="id_role_'+ idx +'_count">Role '+ idx +' count:</label> <input type="number" name="role_'+ idx +'_count" min="1" id="id_role_'+ idx +'_count"> </p>');
+        }
+    });
+
+});
