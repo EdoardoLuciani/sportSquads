0839b835c99d442e2690a656e5496157501ff944
Edoardo Luciani <edoardo.luciani@gmail.com>
2022-03-18 19:53:33 +0000

    Add progress bar for each role in team page

--- static/js/team.js
+++ static/js/team.js
@@ -1,122 +1,16 @@
-//$(document).ready(function() { 
-
-//dict(role,quantity){
-//   this.role = role;
-//   this.quantity = quantity;
-// }
-
-
-
-
-// var elem = document.getElementById("roles");
-// var roles = document 
-// for (var i=0; i<dict.length-1; i++){
-//   var dict = new Object();
-//   point.role = role;
-//   point.quantity = value;
-
-// }
-
-
-
-// var i = 0;
-// function move() {
-//   if (i == 0) {
-//     i = 1;
-//     var elem = document.getElementById("myBar");
-//     var width = 100/dict.role.quantity;
-//     var id = setInterval(frame, 10);
-//     function frame() {
-//       if (width >= 100) {
-//         clearInterval(id);
-//         i = 0;
-//       } else {
-//         width++;
-//         elem.style.width = width + "%";
-//       }
-//     }
-//   }
-// }
-
-
-// sport_data = data["sport"];
-// teams_data = data["teams"];
-// document.write("test");
-// for (var i = 0; i < sport_data.length; i++) {
-//   ('<div id="sport"> <a href="/sport/' + sport_data.name_slug + '">' + sport_data.roles + '</a> </div>');
-//   for (var j = 0; j < sports_data.roles.length; i++) {
-//     for (var k = 0; k < sports_data.roles[j]; i++) {
-//       var elem = document.getElementById("myBar");
-//       var width = 100/sports_data.roles[j][k];
-//       var id = setInterval(frame, 10);
-//       function frame() {
-//       if (width >= 100) {
-//         clearInterval(id);
-//         i = 0;
-//       } else {
-//         width++;
-//         elem.style.width = width + "%";
-//         }
-//       }
-
-//       for (var i = 0; i < teams_data.length; i++) {
-//         for (var i = 0; i < teas_data.members_with_roles.length; i++){
-//           if (teas_data.members_with_roles[i][1] == sports_data.roles[j]){
-//             frame();
-//           }
-//         }
-
-//       }
-//     }
-//   }
-// }
-
-//}
-
-
-// $(document).ready(function(){}
-
-// var counter = 0;
-// for (role in sports.roles.items){
-//   for (member in  teams.members_with_roles){
-//     if (member.role == role){
-//       counter = counter +1 ;
-//     }
-//   }
-// }
-// document.getElementById('output').innerHTML = counter;
-
-
-
-
-// var elem = document.getElementById("a");
-
-// var tot_val =  {{sports.roles.value}} - {{ teams.members_with_roles[role]}}
-// var current_val = 0
-
-// doc.append(<progress id="file" max=tot_val value=current_val> 70% </progress>);
-
-
 $(document).ready(function() {
-//capacity per role    
-var data = capacity;
-//member names
-var data2 = m;
-var data3 = availability;
-console.log(availability)
-console.log(capacity);
-var interval = $('#pagejs_general_delete_wizardresultid').attr('data');;
+    team_json_available_roles = JSON.parse(team_json_available_roles.replace(/'/g, '"'));
+    sport_json_roles = JSON.parse(sport_json_roles.replace(/'/g, '"'));
 
-var progression = 0,
-    progress = setInterval(function() 
-    {
-        $('#progress .progress-text').text(progression + '%');
-        $('#progress .progress-bar').css({'width':progression+'%'});
-        if(progression == capacity) {
-        
-        } else
-            progression += (capacity-availability);
+    for (var key in team_json_available_roles) {
+        var progress = ((sport_json_roles[key] - team_json_available_roles[key])/parseFloat(sport_json_roles[key]))*100;
+        $('#' + key + '-bar').attr('value', progress);
+        $('#' + key + '-bar').text(progress + '%');
 
-    }, 1000);
- });
+        $('#' + key + '-label').text(progress + '%');
+    }
+
+    console.log(team_json_available_roles);
+    console.log(sport_json_roles);
+});
 
