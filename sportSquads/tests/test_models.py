a004527cccccc95ada26b04520ecb1e22cf70e2f
Edoardo Luciani <edoardo.luciani@gmail.com>
2022-03-22 00:53:36 +0000

    Test user without pfp

--- sportSquads/tests/test_models.py
+++ sportSquads/tests/test_models.py
@@ -1,27 +1,55 @@
 import os
+from django.conf import settings
 from django.test import TestCase
 from sportSquads.models import *
 from django.core.files import File
 
 TEST_FILES_DIR = os.path.join(os.path.dirname(__file__), 'test_files')
 
-ALL_TEST_FILES = [ os.path.join(TEST_FILES_DIR, f) for f in os.listdir(TEST_FILES_DIR) ]
+ALL_TEST_FILE_NAMES = [ f for f in os.listdir(TEST_FILES_DIR) ]
+
+def find_and_remove_files(dir, files_to_remove):
+    local_directories = []
+
+    for elem_name in os.listdir(dir):
+        elem_path = os.path.join(dir, elem_name)
+
+        if elem_name in files_to_remove:
+            os.remove(elem_path)
+            files_to_remove.remove(elem_name)
+        elif os.path.isdir(elem_path):
+            local_directories.append(elem_path)
+    
+    if (len(files_to_remove) > 0):
+        for local_dir in local_directories:
+            find_and_remove_files(local_dir, files_to_remove)
 
 def cleanup():
-    for file_name in ALL_TEST_FILES:
-        # get filename removing the path
-        os.remove(file_name)
+    find_and_remove_files(settings.MEDIA_DIR, ALL_TEST_FILE_NAMES.copy())
+        
 
 class UserProfileTestCase(TestCase):
     def setUp(self):
         cleanup()
 
-    def test_user_profile_creation(self):
+    def tearDown(self):
+        cleanup()
+
+    def test_user_profile_creation_with_pfp(self):
         user = User.objects.create(username="testuser", password="testpassword")
 
         with open(os.path.join(TEST_FILES_DIR, "test_pfp.jpg"), "rb") as f:
             user_profile = UserProfile.objects.create(user=user, profile_picture=File(f, name="test_pfp.jpg"), bio="test bio")
 
         self.assertEqual(user_profile.user.username, "testuser")
+        self.assertEqual(user_profile.user.password, "testpassword")
         self.assertEqual(user_profile.profile_picture.url, "/media/profile_images/test_pfp.jpg")
         self.assertEqual(user_profile.bio, "test bio")
+
+    def test_user_profile_creation_without_pfp(self):
+        user = User.objects.create(username="testuser", password="testpassword")
+        user_profile = UserProfile.objects.create(user=user, profile_picture=None)
+
+        self.assertEqual(user_profile.user.username, "testuser")
+        self.assertEqual(user_profile.profile_picture, None)
+        self.assertEqual(user_profile.bio, '')
