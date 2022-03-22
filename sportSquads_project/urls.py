bae02310f3fbfb980005b1d3621ec85ff828518d
Edoardo Luciani <edoardo.luciani@gmail.com>
2022-03-22 19:23:09 +0000

    Create view manage for team

--- sportSquads_project/urls.py
+++ sportSquads_project/urls.py
@@ -31,11 +31,14 @@ urlpatterns = [
     path('search-teams', views.search_teams, name='search_teams'),
     path('contact-us', views.contact_us, name='contact_us'),
     path('login/', views.user_login, name='login'),
+
     path('sport/<slug:sport_name_slug>/', views.show_sport, name='show_sport'),
     path('sport_get_10_more_teams/<slug:sport_name>/<int:starting_team_no>', views.sport_get_10_more_teams, name='sport_get_teams'),
 
     path('team/<slug:team_name_slug>/', views.show_team, name='show_team'),
+    path('team/<slug:team_name_slug>/manage', views.manage_team, name='manage_team'),
     path('join-team/<slug:team_name>/', views.join_team, name='join_team'),
+
     path('account', views.account_information, name='account_information'),
     path('add-sport/', views.add_new_sport, name='add_new_sport'),
     path('logout/', views.user_logout, name='logout'),
